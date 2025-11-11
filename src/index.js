762fd02feab2ae9998eda30fabae7316c52805a4
Aniket-8800 <aniketkambe@ssgc.asia>
2025-11-11 13:21:53 +0530

    DB setup and connect

---
+++ src/index.js
@@ -0,0 +1,41 @@
+// require('dotenv').config({path:'./env'});
+
+//resently intoduced ES6 module syntax
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+
+//we can use as the exprimential feature of ES6 module syntax
+dotenv.config({path:"./env"})
+
+
+connectDB();
+
+// ;(
+//     async()=>{
+
+//         try {
+
+//             await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+            
+//             // Event listeners for successful connection and errors
+//             app.on("error",(error)=>{
+//                 console.error("Failed to connect to the database", error);
+//                 process.exit(1);
+//             })
+
+//             app.listen(process.env.PORT,()=>{
+//                 console.log(`Server is running on port ${process.env.PORT}`);
+//             })
+
+
+            
+//         } catch (error) {
+//             console.error("Error connecting to the database:", error);
+//             throw error;
+            
+//         }
+
+//     }
+
+// )()
