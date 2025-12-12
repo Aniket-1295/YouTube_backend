40850630f9f79eceafc79d8641f5b32cc23291a8
Aniket-1295 <aniketkamble1642002@gmail.com>
2025-12-12 09:38:01 +0530

    Add new secured user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import {Router} from "express";
-import {regesterUser,loginUser,logoutUser,refereshAccessToken} from "../controllers/user.controller.js"
+import {regesterUser,
+    loginUser,
+    logoutUser,
+    refereshAccessToken,
+    changePassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+} from "../controllers/user.controller.js"
 const router = Router();
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -21,10 +32,31 @@ router.route("/regester").post(
 
 router.route("/login").post(loginUser)
 
-//secured routes 
-//take care of the oreder of the middleware
+//secured routes (only logged in users can access these routes)
+
+
 router.route("/logout").post( verifyJWT,logoutUser)
 
 router.route("/refresh-token").post(refereshAccessToken) //http://localhost:5000/api/v1/users/refresh-token")
+
+
+router.route("/change-password").post(verifyJWT,changePassword) //http://localhost:5000/api/v1/users/change-password")
+
+
+router.route("/current-user").get(verifyJWT,getCurrentUser) //http://localhost:5000/api/v1/users/get-current-user")
+
+routet.route("/update-account").patch(verifyJWT,updateAccountDetails) //http://localhost:5000/api/v1/users/update-account-details")
+
+
+//take care of the oreder of the middlewares
+router.route("/update-user-avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar) //http://localhost:5000/api/v1/users/update-user-avatar")
+
+router.route("/update-user-cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage) //http://localhost:5000/api/v1/users/update-user-cover-image"
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile) //http://localhost:5000/api/v1/users/get-user-channel-profile")
+
+router.route("/watch-history").get(verifyJWT,getWatchHistory) //http://localhost:5000/api/v1/users/get-watch-history")
+
+
 
 export default router;
